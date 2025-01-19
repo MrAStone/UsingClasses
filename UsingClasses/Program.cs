cf200abb24f218ca8c805326e5d153fa763cb4f1
Al Stone <astone@heckgrammar.co.uk>
2025-01-19 16:15:56 +0000

    Add project files.

---
+++ UsingClasses/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace UsingClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoClass demo = new DemoClass("Hello", 42);
+            demo.Print();
+            Console.WriteLine(demo.GetNumber());
+            
+        }
+    }
+}
---
+++ UsingClasses/PublicClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingClasses
+{
+    internal class DemoClass
+    {
+        private string thing;
+        private int number;
+        public DemoClass(string thing, int number)
+        {
+            this.thing = thing;
+            this.number = number;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Thing: {thing}, Number: {number}");
+        }
+        public int GetNumber()
+        {
+            return number;
+        }
+    }
+
+}
